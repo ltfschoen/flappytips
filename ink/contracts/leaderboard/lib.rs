3c793b113106b7e5abfe132b8df2f9104b69c204
Luke Schoen <ltfschoen@gmail.com>
2020-07-04 15:12:14 +0200

    setup set account scores

--- ink/contracts/leaderboard/lib.rs
+++ ink/contracts/leaderboard/lib.rs
@@ -47,30 +47,29 @@ mod leaderboard {
             value
         }
 
-        // Set the score for the calling AccountId
+        // Set the score for a given AccountId
         #[ink(message)]
-        fn set_score_of_sender(&self, score: u32) -> () {
-            let caller = self.env().caller();
-            match self.account_map.get(&caller) {
+        fn set_score_of_account(&mut self, of: AccountId, score: u32) -> () {
+            match self.account_map.get(&of) {
                 Some(_) => {
-                    self.account_map.mutate_with(&caller, |value| *value += score);
+                    self.account_map.mutate_with(&of, |value| *value = score);
                 }
                 None => {
-                    self.account_map.insert(caller, score);
+                    self.account_map.insert(of, score);
                 }
             };
         }
 
-        // Set the score for a given AccountId
+        // Set the score for the calling AccountId
         #[ink(message)]
-        fn set_score_of_account(&self, of: AccountId, score: u32) -> () {
+        fn set_score_of_sender(&mut self, score: u32) -> () {
             let caller = self.env().caller();
-            match self.account_map.get(&of) {
+            match self.account_map.get(&caller) {
                 Some(_) => {
-                    self.account_map.mutate_with(&of, |value| *value += score);
+                    self.account_map.mutate_with(&caller, |value| *value = score);
                 }
                 None => {
-                    self.account_map.insert(of, score);
+                    self.account_map.insert(caller, score);
                 }
             };
         }
@@ -84,14 +83,6 @@ mod leaderboard {
         }
     }
 
-    // Free Functions
-
-    /// Returns a dummy AccountId for unit tests
-    fn get_dummy_account() -> AccountId {
-        let account: AccountId = [0u8; 32].into();
-        account
-    }
-
     /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
     /// module and test functions are marked with a `#[test]` attribute.
     /// The below code is technically just normal Rust code.
@@ -100,21 +91,37 @@ mod leaderboard {
         /// Imports all the definitions from the outer scope so we can use them here.
         use super::*;
 
-        #[test]
-        fn constructor_new_works() {
-            let leaderboard = Leaderboard::new();
+        // Free Functions
+
+        /// Returns a dummy AccountId for unit tests
+        fn get_dummy_account() -> AccountId {
+            [0u8; 32].into()
         }
 
         #[test]
         fn get_score_of_account_works() {
-            let mut leaderboard = Leaderboard::new();
+            let leaderboard = Leaderboard::new();
             assert_eq!(leaderboard.get_score_of_account(get_dummy_account()), 0);
         }
 
         #[test]
         fn get_score_of_sender_works() {
-            let mut leaderboard = Leaderboard::new();
+            let leaderboard = Leaderboard::new();
             assert_eq!(leaderboard.get_score_of_sender(), 0);
+        }
+
+        #[test]
+        fn set_score_of_sender_works() {
+            let mut leaderboard = Leaderboard::new();
+            assert_eq!(leaderboard.set_score_of_sender(1), ());
+            assert_eq!(leaderboard.get_score_of_sender(), 1);
+        }
+
+        #[test]
+        fn set_score_of_account_works() {
+            let mut leaderboard = Leaderboard::new();
+            assert_eq!(leaderboard.set_score_of_account(get_dummy_account(), 2), ());
+            assert_eq!(leaderboard.get_score_of_account(get_dummy_account()), 2);
         }
     }
 }
