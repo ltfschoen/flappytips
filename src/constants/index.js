90eec159753700b206d14439d93b358a846eaa5b
Luke Schoen <ltfschoen@gmail.com>
2020-07-06 23:21:23 +0200

    remove comments

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,6 @@
 const ENDPOINTS = {
   'Kusama': 'wss://cc3-5.kusama.network/',
   'Polkadot-CC1': 'wss://cc1-1.polkadot.network',
-  // Note: Edgeware not supported as it returns error
-  // `Unhandled Rejection (Error): createType(Vec<EventRecord>):: Struct: failed on 'data':: Unable to create Enum via index 20, in Normal, Operational, Mandatory`
   'Edgeware Mainnet': 'wss://mainnet1.edgewa.re',
   'DataHighway Harbour Testnet': 'wss://testnet-harbour.datahighway.com',
   // Note: Westend not supported as it does not use Treasury, which is required for using Tips
